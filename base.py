0e7373254aa05a09b38e8aadfc9f132d0fa26940
theealienboy <mrsimonduffy@hotmail.com>
2018-10-02 22:16:43 +0100

    Update base.py

--- base.py
+++ base.py
@@ -15,3 +15,24 @@ remoteServerIP  = socket.gethostbyname(remoteServer)
 print ("-" * 60)
 print ("Fan socind amhain"), remoteServerIP
 print ("-") * 60
+
+try:
+    for port in range(1,1025):  
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print "Port {}: 	 Open".format(port)
+        sock.close()
+
+except KeyboardInterrupt:
+    print "bhrúigh tú Ctrl+C"
+    sys.exit()
+
+except socket.error:
+    print 'Ní fheicall an rionamire. Tá mé ag imeacht'
+    sys.exit()
+
+except socket.error:
+    print "Níl me abalta "
+    sys.exit()
+
