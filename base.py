a701149e5991e097cc55956961f9ad50fcd5cf13
theealienboy <mrsimonduffy@hotmail.com>
2018-10-02 22:06:50 +0100

    Update base.py

--- base.py
+++ base.py
@@ -10,3 +10,8 @@ subprocess.call('clear', shell=True)
 # Cuir teacs anseo
 remoteServer    = raw_input("Riomhaire iargúlta:")
 remoteServerIP  = socket.gethostbyname(remoteServer)
+
+# priontáil bratach deas
+print "-" * 60
+print "Fan socind amhain", remoteServerIP
+print "-" * 60
