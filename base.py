5b13b46ec7541a0e94065efdf210a328ac25c453
theealienboy <mrsimonduffy@hotmail.com>
2018-10-03 15:59:18 +0100

    Update base.py

--- base.py
+++ base.py
@@ -4,35 +4,54 @@ import subprocess
 import sys
 from datetime import datetime
 
-# glan suas an teacs
+# Clear the screen
 subprocess.call('clear', shell=True)
 
-# Cuir teacs anseo
-remoteServer    = input("Riomhaire iargúlta: ")
+# Ask for input
+remoteServer    = raw_input("Riomhaire iargúlta: ")
 remoteServerIP  = socket.gethostbyname(remoteServer)
 
-# priontáil bratach deas
-print ("-" * 60)
-print ("Fan socind amhain"), remoteServerIP
-print ("-") * 60
+# Print a nice banner with information on which host we are about to scan
+print "-" * 60
+print "Fan socind amhain, le do thoil", remoteServerIP
+print "-" * 60
+
+# Check what time the scan started
+t1 = datetime.now()
+
+# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+
+# We also put in some error handling for catching errors
+
+#for port in range(0,65535):
 
 try:
-    for port in range(1,1025):  
+    for port in range(0,65535):  
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
-            print "Port {}: 	 Open".format(port)
+            print "Port {}:   Open".format(port)
         sock.close()
 
 except KeyboardInterrupt:
-    print "bhrúigh tú Ctrl+C"
+    print "Bhrúigh tú Ctrl+C"
+    sys.exit()
+
+except socket.gaierror:
+    print 'Níl a fhios agam an riomhaire. Tá muid ag imeacht'
     sys.exit()
 
 except socket.error:
-    print 'Ní fheicall an rionamire. Tá mé ag imeacht'
+    print "Tá fadhbanna againn"
     sys.exit()
 
-except socket.error:
-    print "Níl me abalta "
-    sys.exit()
+# Checking the time again
+t2 = datetime.now()
+
+# Calculates the difference of time, to see how long it took to run the script
+total =  t2 - t1
+
+# Printing the information to screen
+print 'Scanning Completed in: ', total
+
 
